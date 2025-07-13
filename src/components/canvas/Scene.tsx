c7d64bb2d12c6b0a08f61469f54e346c1cc6c9e3
Erik Edmonds <erikedmonds2019@u.northwestern.edu>
2025-07-13 11:49:24 -0400

    Menu Button Added

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -4,16 +4,20 @@ import { Canvas } from '@react-three/fiber'
 import { Preload } from '@react-three/drei'
 import { r3f } from '@/helpers/global'
 import * as THREE from 'three'
+import { useState } from 'react'
+import { Menu } from '@/components/dom/Menu'
 
 export default function Scene({ ...props }) {
-  // Everything defined in here will persist between route changes, only children are swapped
+  const [menuOpened, setMenuOpened] = useState(false)
   return (
-    <Canvas {...props}
-      onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
-    >
-      {/* @ts-ignore */}
-      <r3f.Out />
-      <Preload all />
-    </Canvas>
+    <>
+      <Canvas {...props}
+              onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}>
+        {/* @ts-ignore */}
+        <r3f.Out />
+        <Preload all />
+      </Canvas>
+      <Menu menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
+    </>
   )
 }
